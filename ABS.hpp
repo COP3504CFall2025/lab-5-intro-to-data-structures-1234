b77e032853242c2f71a63353476959b60fe712a1
CJ Miller <cjmiller979@gmail.com>
2025-11-14 13:13:00 -0500

    Try 41

--- ABS.hpp
+++ ABS.hpp
@@ -121,7 +121,7 @@ T ABS<T>::pop() {
     }
     curr_size_--;
     T rV = array_[curr_size_];
-    if (curr_size_ <= capacity_/scale_factor_) {
+    if (curr_size_ < capacity_/scale_factor_) {
         capacity_ /= scale_factor_;
         T* temp = new T[capacity_];
         for (size_t i = 0; i < curr_size_; i++) {
